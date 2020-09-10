e0ca81f78c354c2b025152be65e013bd4dfc4f40
Rakesh Aravelly <rakesh.aravelly@gmail.com>
2020-09-10 15:44:02 +1000

    added unit test for api controller

---
+++ LifeCover/LifeCover.Api.UnitTests/PremiumControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using LifeCover.Api.Controllers;
+using LifeCover.Api.Models;
+using LifeCover.Api.Validators;
+using LifeCover.Services;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace LifeCover.Api.UnitTests
+{
+    [TestFixture]
+    public class PremiumControllerTests
+    {
+        private PremiumController sut;
+        private IPremiumCalculationService service;
+        private IValidator<LifePremiumModel> validator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            service = Substitute.For<IPremiumCalculationService>();
+            validator = Substitute.For<IValidator<LifePremiumModel>>();
+            sut = new PremiumController(service, validator);
+        }
+
+        [Test]
+        public void Should_Return_Bad_Request()
+        {
+            // Arrange
+            
+            validator.Validate(Arg.Any<LifePremiumModel>()).Returns(new LifePremiumModelValidator().Validate(new LifePremiumModel()));
+
+            // Act
+            var actionResult = sut.CalculatePremium(new LifePremiumModel());
+
+            // Assert
+            Assert.NotNull(actionResult);
+            Assert.IsInstanceOf<BadRequestObjectResult>(actionResult.Result);
+        }
+    }
+}
